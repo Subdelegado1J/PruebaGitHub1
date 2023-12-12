3b23fce955913e92b173e484e22d0b72b97cf70b
Subdelegado1J <juaespvaz@alu.edu.gva.es>
2023-12-12 09:44:59 +0100

    Primer commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int hola = 4;
+        int hola2 = 5;
+
+        System.out.println("Resultado de la suma: " + hola + hola2);
     }
 }
